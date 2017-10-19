c177f32ed06738e7e078547ac7707cfd2f3dc5d9
Luiz Velloso <Luiz_henrique_velloso@hotmail.com>
2017-10-18 23:04:46 -0200

    Lannister House

---
+++ 02 clase/Lanister.java
@@ -0,0 +1,8 @@
+public class Lannister {
+	private String motto = "A Lanister always pays his debts";
+	private int militaryForce = 20000;
+	private String simbol = "Lion";
+	private String location = "Red Keep";
+	private String heir = "Jaime Lanister";
+	private String religion = "Faith of the Seven"
+}
